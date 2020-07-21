8a4291869a90cf3521abdde635acc293ee60e06b
Samuel Reeve <sreeve96@googlemail.com>
2020-07-21 19:38:18 +0100

    Set up basic controls (#28)

--- Extension/extensionSrc/contentScript/src/index.tsx
+++ Extension/extensionSrc/contentScript/src/index.tsx
@@ -3,122 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // ========================
-// React components
-// ========================
-
-function CardsContainer(props: any) {
-    const [annotations, setAnnotations] = React.useState(props.storageAnnotations as annotation[]);
-
-    let newAnnotation: annotation = {
-        id: annotations.length,
-        comment: Lorum,
-        created: Date.now().toString(),
-        colour: colourArray[Math.floor(Math.random() * colourArray.length)],
-        userProfileURL: '',
-        userInitials: '??'
-    }
-
-    function addDummyAnnotation() {
-        setAnnotations(annotations.concat([newAnnotation]));
-    }
-
-    function handleRemoveAnnotation(annotationId: number) {
-        let deleteConfirmed: boolean = window.confirm('Are you sure you want to delete this annotation?');
-        if (deleteConfirmed) {
-            //var annotationsClone: annotation[] = annotations;
-            //var annotationToDeleteIndex = searchObjectArray(annotationsClone, annotationId, 'id')
-            //setAnnotations(annotationsClone);
-            alert('blam');
-        }
-    }
-
-    const cards = annotations.map((annotation) => {
-        return (
-            <AnnotationCard
-                annotationData={annotation}
-                key={annotation.id}
-                removeAnnotation={handleRemoveAnnotation}
-            />
-        )
-    })
-
-    return (
-        <div id='shadowDiv'>
-            <h1>Hello, Lets test the cards container</h1>
-            <button
-                id='createNewAnnotation'
-                onClick={() => addDummyAnnotation()}
-            >
-                Create new annotation
-            </button>
-            <ol className='cardsContainer'>
-                {cards}
-            </ol>
-        </div>
-    );
-}
-
-function AnnotationCard(props: any) {
-    const annotationData: annotation = props.annotationData;
-    const [editMode, setEditMode] = React.useState(false as boolean);
-    const [controlsMenuOpen, setControlsMenuOpen] = React.useState(true as boolean);
-
-    return (
-        <li
-            className={`annotationCard '${editMode ? 'edit' : ''}`}
-            style={{ backgroundColor: `${annotationData.colour}` }}
-        >
-            <div className='userID'>
-                <CardIdentifier
-                    userProfileURL={annotationData.userProfileURL}
-                    userInitials={annotationData.userInitials}
-                />
-            </div>
-            <div className='cardMain'>
-                <div className='cardHeader'>
-                    <div className='cardTitle'>
-                        <p className='username'>User Name</p>
-                        <p className='created'>Created just now</p>
-                    </div>
-                    <div className='controls'>
-                        <p title='Edit' onClick={() => { setEditMode(!editMode) }}>
-                            Edit
-                        </p>
-                        <p title='Delete' onClick={() => { props.removeAnnotation() }}>
-                            Delete
-                        </p>
-                            {/* 
-                            
-                            Will props move this out of this menu? Need to think about it when its working
-                            
-                            <p title='Begin Thread (Coming soon!)'>
-                            &#128284;
-                            </p> */}
-                    </div>
-                </div>
-                <div className='cardBody'>
-                    <textarea disabled={!editMode}>
-                        {annotationData.id + ' ' + annotationData.comment + ' ' + annotationData.created}
-                    </textarea>
-                </div>
-            </div>
-        </li>
-    )
-}
-
-function CardIdentifier(props: any) {
-    const userProfileURL: string = props.userProfileURL;
-    const userInitials: string = props.userInitials;
-
-    if (userProfileURL !== '') {
-        return <img alt='userProfile' src={userProfileURL}></img>
-    }
-    else {
-        return <p className='initials'>{userInitials}</p>
-    }
-}
-
-// ========================
 // Acetate Initialisation
 // ========================
 // Create a shadow DOM
@@ -148,6 +32,140 @@ function createCardContainer() {
 }
 
 // ========================
+// React components
+// ========================
+
+function CardsContainer(props: any) {
+    const [annotations, setAnnotations] = React.useState(props.storageAnnotations as annotation[]);
+
+    let newAnnotation: annotation = {
+        id: annotations.length,
+        comment: blamString,
+        created: new Date(Date.now()),
+        colour: colourArray[Math.floor(Math.random() * colourArray.length)],
+        userProfileURL: '',
+        userName: 'Test Name'
+    }
+
+    function addDummyAnnotation() {
+        setAnnotations(annotations.concat([newAnnotation]));
+    }
+
+    function deleteAnnotation(annotationId: number) {
+        let deleteConfirmed: boolean = window.confirm('Are you sure you want to delete this annotation?');
+        if (deleteConfirmed) {
+            var annotationsClone: annotation[] = annotations.filter(annotation => annotation.id !== annotationId);
+            setAnnotations(annotationsClone);
+        }
+    }
+
+    const cards = annotations.map((annotation) => {
+        return (
+            <AnnotationCard
+                annotationData={annotation}
+                key={annotation.id}
+                annotationMethods={{ deleteAnnotation}} //, saveAnnotationComment }}
+            />
+        )
+    })
+
+    return (
+        <div id='shadowDiv'>
+            <ol className='cardsContainer'>
+                {cards}
+            </ol>
+        </div>
+    );
+}
+
+function AnnotationCard(props: any) {
+    let annotationData: annotation = props.annotationData;
+    const deleteAnnotation = (annotationId: number) => props.annotationMethods.deleteAnnotation(annotationId);
+    const [editMode, setEditMode] = React.useState(false as boolean);
+
+    function extractInitials() {
+        var userName = annotationData.userName;
+        var matches = checkNullableObject(userName.match(/\b(\w)/g));
+        return matches.join('');
+    }
+
+    function saveChanges(newComment: string) {
+        saveAnnotationComment(newComment);
+        setEditMode(false);
+    }
+    
+    function saveAnnotationComment(newComment: string) {
+        let annotationClone = annotationData;
+        annotationClone.comment = newComment;
+
+        annotationData = annotationClone;
+    }
+
+    return (
+        <li
+            className={`annotationCard '${editMode ? 'edit' : ''}`}
+            style={{ backgroundColor: `${annotationData.colour}` }}
+        >
+            <div className='userID'>
+                <CardIdentifier
+                    userProfileURL={annotationData.userProfileURL}
+                    userInitials={extractInitials()}
+                />
+            </div>
+            <div className='cardMain'>
+                <div className='cardHeader'>
+                    <div className='cardTitle'>
+                        <p className='username'>{annotationData.userName}</p>
+                        <p className='created'>Created: {annotationData.created.toLocaleDateString()}</p>
+                    </div>
+                    <div className='controls'>
+                        <p
+                            title='Edit'
+                            onClick={
+                                () => { editMode ? saveChanges('Need to get the text of the text box, right?') : setEditMode(true) }
+                            }
+                        >
+                            {editMode ? 'Save' : 'Edit'}
+                        </p>
+                        <p
+                            title='Delete'
+                            onClick={() => { deleteAnnotation(annotationData.id) }}
+                        >
+                            Delete
+                        </p>
+                        {/* 
+                            
+                            Will props move this out of this menu? Need to think about it when its working
+                            
+                            <p title='Begin Thread (Coming soon!)'>
+                            &#128284;
+                            </p> */}
+                    </div>
+                </div>
+                <div className='cardBody'>
+                    <textarea 
+                        disabled={!editMode}
+                        value={annotationData.comment}
+                    />
+                </div>
+            </div>
+        </li>
+    )
+}
+
+function CardIdentifier(props: any) {
+    const userProfileURL: string = props.userProfileURL;
+    const userInitials: string = props.userInitials;
+
+    if (userProfileURL !== '') {
+        return <img alt='userProfile' src={userProfileURL}></img>
+    }
+    else {
+        return <p className='initials'>{userInitials}</p>
+    }
+}
+
+// ========================
 // Acetate Functions
 // ========================
 
@@ -155,9 +173,9 @@ function createCardContainer() {
 type annotation = {
     id: number,
     comment: string,
-    created: string,
+    created: Date,
     colour: string,
-    userInitials: string,
+    userName: string,
     userProfileURL: string
 }
 
@@ -166,13 +184,13 @@ let emulatedStorageAnnotations: annotation[] = [];
 let colourArray: string[] = ['#6c0097', '#ec922a', '#0ec28c', ''];
 
 // Add a couple fake annotations to the emulated storage
-for (let i: number = 0; i < 5; i++) {
+for (let i: number = 0; i < 20; i++) {
     let newAnnotation: annotation = {
         id: i,
         comment: 'Blam',
-        created: Date.now().toString(),
+        created: new Date(Date.now()),
         colour: colourArray[Math.floor(Math.random() * colourArray.length)],
-        userInitials: 'SR',
+        userName: 'Sam J. Reeve',
         userProfileURL: ''
     }
     emulatedStorageAnnotations.push(newAnnotation);
@@ -191,21 +209,11 @@ function checkNullableObject(nullableObject: any) {
     }
 }
 
-//Object array searcher
-function searchObjectArray(array: any[], searchValue: any, objectAttributeName: string) {
-    let index: number = 0
-    for (index; index < array.length; index++) {
-
-    }
-    return index;
-}
-
-const Lorum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-qui officia deserunt mollit anim id est laborum.`
+const blamString = `blam blam blam blam blam blam blam blam.
+blam blam blam blam blam blam blam blam.
+blam blam blam blam blam blam blam blam.
+blam blam blam blam blam blam blam blam.
+blam blam blam blam blam blam blam blam.
+blam blam blam blam blam blam blam blam.`
 
 createCardContainer();
