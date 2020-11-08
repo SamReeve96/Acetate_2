d728addc12ae46337c1f85f3ac8e4f8d4dbe47a7
Samuel Reeve <sreeve96@googlemail.com>
2020-11-08 14:26:31 +0000

    Clear the slate

--- Extension/extensionSrc/reactComponents/src/shared.ts
+++
@@ -1,8 +0,0 @@
-// Nullable object checker (For typescript Vs. e.g. Document object)
-export function checkNullableObject(nullableObject: any): any {
-    if (nullableObject === null) {
-        console.error('Object was found to be null, it cant be: ');
-    } else {
-        return nullableObject;
-    }
-}
